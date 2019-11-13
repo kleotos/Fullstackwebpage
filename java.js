5e6e825071ede3e790a146577ae800dafe230295
kleotos <kleotos@gmail.com>
2019-11-13 22:37:53 +0100

    add java.js

---
+++ java.js
@@ -0,0 +1,12 @@
+/*when the user presses the submit button,change the greeting to include their name */
+
+// get access to the button and set up event handler
+    var button=document.getElementById("submit");
+        button.onclick=changeGreeting;
+
+    function changeGreeting(){
+      var greeting=document.getElementsBytagName("hello,world!");
+
+      var input=document.getElementById("Name"),value
+        greeting.innerHTML="Hello,"+ input;
+    }
